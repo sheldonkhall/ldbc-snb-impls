53f55474afba0b2690e8aa8a3211382570afebbf
Michelangelo Bucci <michelangelobucci@gmail.com>
2017-04-06 18:13:44 +0100

    Add first query stub

--- snb-interactive-grakn/src/main/java/GraknShortQueryHandlers.java
+++ snb-interactive-grakn/src/main/java/GraknShortQueryHandlers.java
@@ -4,6 +4,7 @@ import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfile;
 
+
 /**
  * Created by miko on 06/04/2017.
  * Ruined by felix on 06/04/2017.
@@ -14,12 +15,29 @@ public class GraknShortQueryHandlers {
     public static class LdbcShortQuery1PersonProfileHandler
             implements OperationHandler<LdbcShortQuery1PersonProfile, GraknDbConnectionState>
     {
+
         @Override
         public void executeOperation(LdbcShortQuery1PersonProfile operation,
                                      GraknDbConnectionState dbConnectionState,
                                      ResultReporter resultReporter) throws DbException
         {
             GraknSession session = dbConnectionState.session();
+            String query =
+                    "match" +
+                            "$person isa person has ID " +
+                            "'" + operation.personId() + "' " +
+                            "has first-name $first-name" +
+                            "has last-name $last-name" +
+                            "has birth-day $birthday" +
+                            "has location-ip $location-ip" +
+                            "has browser-used $browser-used" +
+                            "has gender $gender" +
+                            "has creation-date $creation-date;" +
+                            "($person, $placeId) isa located-in;";
+
+            // Execute the query and get the results
+
+            // Parse the result text
         }
     }
 }
