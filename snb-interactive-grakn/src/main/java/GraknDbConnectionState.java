8b565a103e4106023687dcc106941729cdc80570
Michelangelo Bucci <michelangelobucci@gmail.com>
2017-04-06 18:38:20 +0100

    Make dbConnectionState return graphs

--- snb-interactive-grakn/src/main/java/GraknDbConnectionState.java
+++ snb-interactive-grakn/src/main/java/GraknDbConnectionState.java
@@ -1,4 +1,6 @@
+import ai.grakn.GraknGraph;
 import ai.grakn.GraknSession;
+import ai.grakn.GraknTxType;
 import com.ldbc.driver.DbConnectionState;
 
 import java.io.IOException;
@@ -9,17 +11,20 @@ import java.io.IOException;
 public class GraknDbConnectionState extends DbConnectionState {
 
     private final GraknSession session;
+    private final GraknGraph graph;
 
     GraknDbConnectionState(GraknSession session) {
         this.session = session;
+        this.graph = session.open(GraknTxType.READ);
     }
 
     @Override
     public void close() throws IOException {
+        graph.close();
         session.close();
     }
 
-    GraknSession session() {
-        return session;
+    GraknGraph graph() {
+        return graph;
     }
 }
--- snb-interactive-grakn/src/main/java/GraknShortQueryHandlers.java
+++ snb-interactive-grakn/src/main/java/GraknShortQueryHandlers.java
@@ -1,8 +1,15 @@
+import ai.grakn.GraknGraph;
 import ai.grakn.GraknSession;
+import ai.grakn.GraknTxType;
+import ai.grakn.concept.Concept;
+import ai.grakn.graql.MatchQuery;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfile;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by miko on 06/04/2017.
@@ -19,7 +26,23 @@ public class GraknShortQueryHandlers {
                                      GraknDbConnectionState dbConnectionState,
                                      ResultReporter resultReporter) throws DbException
         {
-            GraknSession session = dbConnectionState.session();
+            GraknGraph graph = dbConnectionState.graph();
+
+            String query =
+                    "match" +
+                            "$person isa person has ID " +
+                            "'" + operation.personId() + "' " +
+                            "has first-name $first-name" +
+                            "has last-name $last-name" +
+                            "has birth-day $birthday" +
+                            "has location-ip $location-ip" +
+                            "has browser-used $browser-used" +
+                            "has gender $gender" +
+                            "has creation-date $creation-date;" +
+                            "($person, $place) isa located-in;" +
+                            "$place has ID $placeID";
+
+            List<Map<String, Concept>> results = graph.graql().<MatchQuery>parse(query).execute();
         }
     }
 }
