97da3161d31af70e7bd9de98ed521f9022dec6b7
Michelangelo Bucci <michelangelobucci@gmail.com>
2017-04-06 19:07:43 +0100

    Untested complete first short-query

--- snb-interactive-grakn/src/main/java/GraknShortQueryHandlers.java
+++ snb-interactive-grakn/src/main/java/GraknShortQueryHandlers.java
@@ -1,12 +1,12 @@
 import ai.grakn.GraknGraph;
-import ai.grakn.GraknSession;
-import ai.grakn.GraknTxType;
+
 import ai.grakn.concept.Concept;
 import ai.grakn.graql.MatchQuery;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfile;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfileResult;
 
 import java.util.List;
 import java.util.Map;
@@ -44,6 +44,23 @@ public class GraknShortQueryHandlers {
                             "$place has ID $placeID";
 
             List<Map<String, Concept>> results = graph.graql().<MatchQuery>parse(query).execute();
+            if (results.size() > 0) {
+                Map<String, Concept> fres = results.get(0);
+                LdbcShortQuery1PersonProfileResult result =
+                        new LdbcShortQuery1PersonProfileResult(
+                                (String) fres.get("first-name").asResource().getValue(),
+                                (String) fres.get("last-name").asResource().getValue(),
+                                (Long) fres.get("birthday").asResource().getValue(),
+                                (String) fres.get("location-ip").asResource().getValue(),
+                                (String) fres.get("browser-used").asResource().getValue(),
+                                (Long) fres.get("placeID").asResource().getValue(),
+                                (String) fres.get("gender").asResource().getValue(),
+                                (Long) fres.get("creation-date").asResource().getValue() );
+
+                resultReporter.report(0, result, operation);
+            } else {
+                resultReporter.report(0, null, operation);
+            }
         }
     }
 }
