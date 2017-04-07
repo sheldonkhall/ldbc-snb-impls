d3837bbd048979a28d778b6bb093a2f56e816bc5
Michelangelo Bucci <michelangelobucci@gmail.com>
2017-04-07 18:59:31 +0100

    register short query handlers

--- snb-interactive-grakn/src/main/java/net/ellitron/ldbcsnbimpls/interactive/grakn/GraknDb.java
+++ snb-interactive-grakn/src/main/java/net/ellitron/ldbcsnbimpls/interactive/grakn/GraknDb.java
@@ -1,10 +1,11 @@
 package net.ellitron.ldbcsnbimpls.interactive.grakn;
 
+import ai.grakn.GraknGraph;
 import com.ldbc.driver.Db;
 import com.ldbc.driver.DbConnectionState;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.control.LoggingService;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfile;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -21,6 +22,12 @@ public class GraknDb extends Db {
         connectionState = new GraknDbConnectionState(properties);
 
         registerOperationHandler(LdbcShortQuery1PersonProfile.class, GraknShortQueryHandlers.LdbcShortQuery1PersonProfileHandler.class);
+        // registerOperationHandler(LdbcShortQuery2PersonPosts.class, GraknShortQueryHandlers.LdbcShortQuery2PersonPostsHandler.class);
+        registerOperationHandler(LdbcShortQuery3PersonFriends.class, GraknShortQueryHandlers.LdbcShortQuery3PersonFriendsHandler.class);
+        registerOperationHandler(LdbcShortQuery4MessageContent.class, GraknShortQueryHandlers.LdbcShortQuery4MessageContentHandler.class);
+        registerOperationHandler(LdbcShortQuery5MessageCreator.class, GraknShortQueryHandlers.LdbcShortQuery5MessageCreatorHandler.class);
+        registerOperationHandler(LdbcShortQuery6MessageForum.class, GraknShortQueryHandlers.LdbcShortQuery6MessageForumHandler.class);
+        registerOperationHandler(LdbcShortQuery7MessageReplies.class, GraknShortQueryHandlers.LdbcShortQuery7MessageRepliesHandler.class);
     }
 
     @Override
